5109b48031da1fb9931163079d5aadb84b63f317
atom177P <71344967+atom177P@users.noreply.github.com>
2022-04-24 23:47:39 +0200

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,25 @@
+var _Date = setInterval(function(){
+            _GetDate();
+        },1);
+function _GetDate(){   
+  var today = new Date();
+            
+  var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+            
+  var element = document.getElementById("currentDate");
+           
+  element.innerHTML = date;
+  }
+
+const isBetween = (length, min, max) => length < min || length > max ? false : true;
+
+const isEmailValid = (email) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+};
+
+const isPasswordSecure = (password) => {
+    const re = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+    return re.test(password);
+};
+
